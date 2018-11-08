148d14a383eb0d7223220cdcbe91b5d9856b8293
Callum Evans <callumevans@outlook.com>
2018-11-08 03:15:48 +0000

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Swimlanes cards="cards" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Swimlanes from "@/components/Swimlanes";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      Swimlanes
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
---
+++ src/components/Swimlanes.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div v-for="lane in lanes">
+            <draggable v-model="cards[lane.id]" :options="{group: 'cards'}" :key="lane.id">
+                <transition-group>
+                    <div class="draggable" v-for="element in cards" :key="element.id">
+                        {{element.name}}
+                    </div>
+                </transition-group>
+            </draggable>
+        </div>
+    </div>
+</template>
+
+<script>
+    import draggable from 'vuedraggable'
+    export default {
+        name: "Swimlanes",
+        data: () => {
+            return {
+                lanes: [
+                    {
+                        id: 1,
+                        name: "Lane One"
+                    },
+                    {
+                        id: 2,
+                        name: "Lane Two"
+                    }
+                ],
+                cards: [
+                    {
+                        id: 1,
+                        name: "test card"
+                    }
+                ]
+            }
+        },
+        components: {
+            draggable
+        }
+    };
+</script>
+
+<style scoped>
+    .draggable {
+        display: block;
+        width: 100px;
+        height: 70px;
+        background: red;
+        color: white;
+        margin: 1em auto;
+    }
+</style>
